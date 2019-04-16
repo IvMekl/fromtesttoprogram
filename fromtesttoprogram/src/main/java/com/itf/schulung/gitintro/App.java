ac46f053954521f692ccb330f17940f64f607242
Ivo Matic <ivomatic@hotmail.de>
2019-04-16 22:25:14 +0200

    Oh nein

--- fromtesttoprogram/src/main/java/com/itf/schulung/gitintro/App.java
+++ fromtesttoprogram/src/main/java/com/itf/schulung/gitintro/App.java
@@ -6,6 +6,7 @@ public class App
     {
        RationaleZahl r = new RationaleZahl(14,16);
        System.out.println(r);
+       System.out.println("GIT TEST");
 //       r.kuerze();
 //       System.out.println(r);
     }
